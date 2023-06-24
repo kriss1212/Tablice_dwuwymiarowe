ba4deff081129e0d51555991693436f127adfeca
kriss1212 <krzysiekzory1@o2.pl>
2023-06-24 21:14:18 +0200

    Dodaj pliki projektów.

---
+++ Zadanie 1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] tablica = new int[10, 10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                // Wiersze parzyste - wypełnianie rosnąco
+                for (int j = 0; j < 10; j++)
+                {
+                    tablica[i, j] = i * 10 + j + 1;
+                }
+            }
+            else
+            {
+                // Wiersze nieparzyste - wypełnianie malejąco
+                for (int j = 9; j >= 0; j--)
+                {
+                    tablica[i, j] = i * 10 + (9 - j) + 1;
+                }
+            }
+        }
+
+        // Wyświetlanie tablicy
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(tablica[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
